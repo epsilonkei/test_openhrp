007b6ff1aac6a8ee8f8e99f04937c38b8fdf37e7
Khanh Nguyen <nguyen@jsk.imi.i.u-tokyo.ac.jp>
2018-12-12 18:01:32 +0900

    Rearrange CMakeLists.txt Debug for rpyFromRot

--- src/test_openhrp.cpp
+++ src/test_openhrp.cpp
@@ -14,7 +14,7 @@ public:
     test_openhrp() {}
     ~ test_openhrp() {}
     int rpy_from_rot(double* _rot, double* _rpy) {
-        rot = Eigen::Map<hrp::Matrix33>(_rot, 3, 3);
+        rot = Eigen::Map<hrp::Matrix33>(_rot, 3, 3).transpose();
         rpy = hrp::rpyFromRot(rot);
         Eigen::Map<hrp::Vector3>(_rpy, 3) = rpy;
         return 0;
